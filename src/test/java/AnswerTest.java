45967380b6745962012385a8f9048a176b310167
propspero238 <keithc238@gmail.com>
2017-08-03 11:49:12 -0700

    unused class deletion

--- src/test/java/AnswerTest.java
+++ src/test/java/AnswerTest.java
@@ -9,20 +9,18 @@ import static org.junit.Assert.assertThat;
 public class AnswerTest {
 
 
-    protected Answer answer = new Answer();
     protected String rightWindow = "X=5 Y=9 W=10 H=40";
     protected String leftWindow = "X=16 Y=9 W=10 H=40";
 
     @Test
     public void should_return_identical_dimensions_for_single_window() {
-        Window enclosingWindow = answer.getEnclosing(new String[]{rightWindow});
-        Window expected = enclosingWindow;
-        assertThat(enclosingWindow, WindowMatcher.isWindow(expected));
+        Window enclosingWindow = Answer.getEnclosing(new String[]{rightWindow});
+        assertThat(enclosingWindow, WindowMatcher.isWindow(enclosingWindow));
 
     }
     @Test
     public void should_return_enclosing_coordinates() {
-        Window enclosing = answer.getEnclosing(new String[]{rightWindow, leftWindow});
+        Window enclosing = Answer.getEnclosing(new String[]{rightWindow, leftWindow});
         Window expected = Window.fromStringDimensions("X=5 Y=9 W=21 H=40");
         assertThat(enclosing, WindowMatcher.isWindow(expected));
     }
