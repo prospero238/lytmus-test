97203ed2d982e1d52b6ebb65af0e23ed1aa4324f
propspero238 <keithc238@gmail.com>
2017-08-03 12:00:23 -0700

    added another test

--- src/test/java/AnswerTest.java
+++ src/test/java/AnswerTest.java
@@ -9,21 +9,36 @@ import static org.junit.Assert.assertThat;
 public class AnswerTest {
 
 
-    protected String rightWindow = "X=5 Y=9 W=10 H=40";
-    protected String leftWindow = "X=16 Y=9 W=10 H=40";
+    protected String leftWindow = "X=5 Y=9 W=10 H=40";
+    protected String middleWindow = "X=16 Y=9 W=10 H=40";
+    protected String getLeftWindow = "X=6 Y=1 W=1 H=1";
+
 
     @Test
     public void should_return_identical_dimensions_for_single_window() {
-        Window enclosingWindow = Answer.getEnclosing(new String[]{rightWindow});
+        Window enclosingWindow = Answer.getEnclosing(new String[]{leftWindow});
         assertThat(enclosingWindow, WindowMatcher.isWindow(enclosingWindow));
 
     }
     @Test
     public void should_return_enclosing_coordinates() {
-        Window enclosing = Answer.getEnclosing(new String[]{rightWindow, leftWindow});
+        Window enclosing = Answer.getEnclosing(new String[]{leftWindow, middleWindow});
         Window expected = Window.fromStringDimensions("X=5 Y=9 W=21 H=40");
         assertThat(enclosing, WindowMatcher.isWindow(expected));
     }
+
+    @Test
+    public void should_return_closing_over_three_windows() {
+        Window enclosing = Answer.getEnclosing(new String[]{"X=0 Y=1 W=1 H=1", "X=3 Y=2 W=1 H=2", "X=6 Y=1 W=1 H=1"});
+        Window expected = new Window.Builder().withXPosition(0)
+                                              .withYPosition(1)
+                                              .withWidth(7)
+                                              .withHeight(3).build();
+        assertThat(enclosing, WindowMatcher.isWindow(expected));
+
+    }
+
+
 
     @Test
     public void should_implement_expected_toString() {
